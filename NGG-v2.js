c498a4ef463f315b4c7086002448196f5c0fa3f2
Boketto <104163073+Dollarxcade@users.noreply.github.com>
2024-11-28 21:07:36 -0500

    Update NGG-v2.js

--- NGG-v2.js
+++ NGG-v2.js
@@ -3,5 +3,5 @@ const actualNum = Math.floor(Math.random() * 99);
 let score = Math.abs(actualNum - myGuess);
 alert (`your score is ${score}`)
 
-//Last updated June 1, 2022
+//Last updated 2022-06-01
 //Run in node.js
